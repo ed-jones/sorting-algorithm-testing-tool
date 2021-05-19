8e5cebfa3e1d98eeb7d180e724af83217ce2dfc5
Ed Jones <edjones1999@gmail.com>
2021-05-19 19:12:42 +1000

    rewrote sorting algorithm to be generic

---
+++ src/algorithms/InsertionSort.java
@@ -0,0 +1,14 @@
+public class InsertionSort<T> {
+    public Function<T[], Function<T, T, Boolean>, T[]> insertionSort = (input, compare) -> {
+        for (int i = 1; i < input.length; i++) {
+            T value = input[i];
+            int j = i - 1;
+            while (j >= 0 && compare(input[j], value)) {
+                input[j + 1] = input[j];
+                j = j - 1;
+            }
+            input[j + 1] = value;
+        }
+        return input;
+    };
+}
